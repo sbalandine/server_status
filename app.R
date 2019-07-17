4d89e49379b5f1b2ed8b488bd317699ac2e37cad
Zeno Muscarella <zmuscarella@gmail.com>
2019-07-16 23:45:37 -0700

    Create app.R

---
+++ app.R
@@ -0,0 +1,37 @@
+library(shiny)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+
+ui <- fluidPage(
+    titlePanel('Shiny Server Monitor'),
+    plotOutput('user_chart_today')
+)
+
+server <- function(input, output, session) {
+    
+    filter_user_data_today <- reactive({
+        load('sysLoad.RData')
+        
+        Dat %>% 
+            mutate(hour = as.POSIXct(trunc(Time, 'mins'))) %>% 
+            filter(hour >= as.Date(Sys.time())) %>% 
+            group_by(hour, app) %>% 
+            filter(Time == max(Time)) %>% 
+            slice(1) %>% 
+            ungroup %>% 
+            arrange(hour) #%>% 
+            #mutate(hour = datetime_to_timestamp(hour))    
+    })
+    
+    output$user_chart_today <- renderPlot({
+        filter_user_data_today() %>% 
+	    ggplot2::ggplot(data=., aes(x=hour, y=usr, color=app)) + 
+            geom_point() + 
+            geom_smooth(method='loess') +
+            theme_bw()
+    })
+    
+}
+
+shinyApp(ui = ui, server = server)
