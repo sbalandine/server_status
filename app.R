177c36aa9b5420b13c00ffcb2027c1cf597d1dee
Serge Balandine <serge.balandine@epicentre.msf.org>
2022-03-25 17:52:24 +0100

    .

--- app.R
+++ app.R
@@ -1,38 +1,37 @@
 library(shiny)
-library(dplyr)
+library(tidyverse)
 library(lubridate)
-library(ggplot2)
+library(highcharter)
 
 ui <- fluidPage(
-    titlePanel('Shiny Server Monitor'),
-    plotOutput('user_chart_today')
+  titlePanel('Shiny Server Monitor'),
+  highchartOutput('user_chart_today')
 )
 
 server <- function(input, output, session) {
-    
-    filter_user_data_today <- reactive({
-        load('/srv/shiny-server/server_status/sysLoad.RData')
-        
-        Dat %>% 
-            mutate(hour = as.POSIXct(trunc(Time, 'mins'))) %>% 
-            filter(hour >= as.Date(Sys.time())) %>% 
-            group_by(hour, app) %>% 
-            filter(Time == max(Time)) %>% 
-            slice(1) %>% 
-            ungroup %>% 
-            arrange(hour) %>% 
-	    filter(app != 'shiny-server/server_status')
-            #mutate(hour = datetime_to_timestamp(hour))    
-    })
-    
-    output$user_chart_today <- renderPlot({
-        filter_user_data_today() %>% 
-	    ggplot2::ggplot(data=., aes(x=hour, y=usr, color=app)) + 
-            geom_point() + 
-            geom_smooth(method='loess', se=FALSE) +
-            theme_bw()
-    })
-    
+
+  filter_user_data_today <- reactive({
+    load('/home/s-balandine/r.shiny/status/cache/logs.Rdata')
+
+    logs %>%
+      mutate(hour = as.POSIXct(trunc(Time, 'mins'))) %>%
+      filter(hour >= as.Date(Sys.time())) %>%
+      group_by(hour, app) %>%
+      filter(Time == max(Time)) %>%
+      slice(1) %>%
+      ungroup() %>%
+      arrange(hour) %>%
+      mutate(hour = datetime_to_timestamp(hour))
+  })
+
+  output$user_chart_today <- renderHighchart({
+    filter_user_data_today() %>%
+      hchart(hcaes(x = hour, y = usr, group = app), type = 'line') %>%
+      hc_xAxis(type = 'datetime') %>%
+      hc_tooltip(shared = TRUE) %>%
+      hc_add_theme(hc_theme_smpl())
+  })
+
 }
 
 shinyApp(ui = ui, server = server)
--- check_server.R
+++ check_server.R
@@ -1,49 +1,56 @@
-setwd("/srv/shiny-server/server_status")
-RData <- "sysLoad.RData"
+shiny_user <- 's-balandine'
+shiny_dir <- sprintf('/home/%s/r.shiny', shiny_user)
+shiny_dir_cache <- file.path(shiny_dir, 'status', 'cache')
 
-## try to load in R data
-tryCatch(load(RData), 
-         warning = function(c) {return(NULL)}, #'warning on read of load data',
-         error = function(c) {return(NULL)},
-         message = function(c) 'message'
-         )
+dir.create(shiny_dir_cache, showWarnings = FALSE)
 
-## if the above fails then `Dat` doesn't exist so let's set it to null
-if(!exists('Dat')){
-  Dat <- NULL
+setwd(shiny_dir_cache)
+
+file <- 'logs.Rdata'
+
+## try to load in R loga
+tryCatch(load(file),
+  warning = function(c) {return(NULL)}, #' warning on read of load loga',
+  error = function(c) {return(NULL)},
+  message = function(c) 'message'
+)
+
+## if the above fails then `logs` doesn't exist so let's set it to null
+if (!exists('logs')) {
+  logs <- NULL
 }
 
 I <- 0
 
 repeat {
-  system("top -n 1 -b -u shiny > top.log")
-  dat <- readLines("top.log")
-  id <- grep("R *$", dat)
-  Names <- strsplit(gsub("^ +|%|\\+", "", dat[7]), " +")[[1]]
+  system(sprintf('top -n 1 -b -u %s > top.log', shiny_user))
+  log <- readLines('top.log')
+  id <- grep('R *$', log)
+  Names <- strsplit(gsub('^ +|%|\\+', '', log[7]), ' +')[[1]]
   if (length(id) > 0) {
-    # 'top' data frame;
-    L <- strsplit(gsub("^ *", "", dat[id]), " +")
-    dat <- data.frame(matrix(unlist(L), ncol = 12, byrow = T))
-    names(dat) <- Names
-    dat <- data.frame(Time = Sys.time(), dat[, -ncol(dat)], usr = NA, app = NA)
-    dat$CPU <- as.numeric(as.character(dat$CPU))
-    dat$MEM <- as.numeric(as.character(dat$MEM))
+    # 'top' loga frame;
+    L <- strsplit(gsub('^ *', '', log[id]), ' +')
+    log <- data.frame(matrix(unlist(L), ncol = 12, byrow = T))
+    names(log) <- Names
+    log <- data.frame(Time = Sys.time(), log[, -ncol(log)], usr = NA, app = NA)
+    log$CPU <- as.numeric(as.character(log$CPU))
+    log$MEM <- as.numeric(as.character(log$MEM))
     # Check if connection number changed;
-    for (i in 1:length(dat$PID)) {
-      PID <- dat$PID[i]
-      system(paste("sudo netstat -p | grep", PID, "> netstat.log"))
-      system(paste("sudo netstat -p | grep", PID, ">> netstat.log2"))
-      system(paste("sudo lsof -p", PID, "| grep /srv > lsof.log"))
-      netstat <- readLines("netstat.log")
-      lsof <- readLines("lsof.log")
-      dat$usr[i] <- length(grep("ESTABLISHED", netstat) & grep("tcp", netstat))
-      dat$app[i] <- regmatches(lsof, regexec("srv/(.*)", lsof))[[1]][2]
+    for (i in 1:length(log$PID)) {
+      PID <- log$PID[i]
+      system(paste('sudo netstat -p | grep', PID, '> netstat.log'))
+      system(paste('sudo netstat -p | grep', PID, '>> netstat.log2'))
+      system(paste('sudo lsof -p', PID, sprintf('| grep %s > lsof.log', shiny_dir)))
+      netstat <- readLines('netstat.log')
+      lsof <- readLines('lsof.log')
+      log$usr[i] <- length(grep('ESTABLISHED', netstat) & grep('tcp', netstat))
+      log$app[i] <- regmatches(lsof, regexec(sprintf('%s/(.*)', shiny_dir), lsof))[[1]][2]
     }
-    if (!is.null(Dat)) {
-      dat.a <- Dat[which(Dat$Time == max(Dat$Time)),]
-      con.a <- dat.a$usr[order(dat.a$app)]
-      con.b <- dat$usr[order(dat$app)]
-      if (paste(con.a, collapse = "") == paste(con.b, collapse = "")) {
+    if (!is.null(logs)) {
+      log.a <- logs[which(logs$Time == max(logs$Time)), ]
+      con.a <- log.a$usr[order(log.a$app)]
+      con.b <- log$usr[order(log$app)]
+      if (paste(con.a, collapse = '') == paste(con.b, collapse = '')) {
         changed <- FALSE
       } else {
         changed <- TRUE
@@ -52,10 +59,10 @@ repeat {
       changed <- TRUE
     }
     # Keep only the lines containing important information to same storage space;
-    if (any(dat$CPU > 5) | any(dat$MEM > 50) | changed) {
-      Dat <- rbind(Dat, dat)
-      Dat <- Dat[which(Dat$Time > (max(Dat$Time)-30*24*60*60)), ]
-      save(Dat, file = RData)
+    if (any(log$CPU > 5) | any(log$MEM > 50) | changed) {
+      logs <- rbind(logs, log)
+      logs <- logs[which(logs$Time > (max(logs$Time) - 30 * 24 * 60 * 60)), ]
+      save(logs, file = file)
     }
   }
   Sys.sleep(5)
